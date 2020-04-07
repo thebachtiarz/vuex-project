fa3ab1cb27065c81ff97f73ef3b0925da6aa6692
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2020-04-07 14:22:47 +0700

    update - biodata upload image method

--- src/views/Page/User/Profile.vue
+++ src/views/Page/User/Profile.vue
@@ -7,7 +7,7 @@
             <img
               class="profile-user-img img-fluid img-circle"
               :src="`${this.$AppHelper.apiEndpoint()}${thisBiodata.profile_img}`"
-              alt="Student profile image"
+              alt="user-profile-image"
             />
           </div>
           <h3 class="profile-username text-center">{{ thisBiodata.name }}</h3>
@@ -30,14 +30,97 @@
               <a class="nav-link text-bold active" href="#history" data-toggle="tab">History</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-bold" href="#settings" data-toggle="tab">Settings</a>
+              <a class="nav-link text-bold" href="#biodata" data-toggle="tab">Biodata</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-bold" href="#password" data-toggle="tab">Password</a>
             </li>
           </ul>
         </div>
         <div class="card-body">
           <div class="tab-content">
             <div class="tab-pane active" id="history">Login History.</div>
-            <div class="tab-pane" id="settings">User Setting.</div>
+            <div class="tab-pane" id="biodata">
+              <p class="text-bold text-center">--- Biodata Management ---</p>
+              <div class="form-group">
+                <label for="setting-biodata-name">Full Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="setting-biodata-name"
+                  :value="thisBiodata.name"
+                />
+              </div>
+              <div class="form-group">
+                <form
+                  id="setting-biodata-image"
+                  enctype="multipart/form-data"
+                  @submit="uploadImageProfile"
+                >
+                  <label for="setting-biodata-image-import">Image Profile</label>
+                  <div class="custom-file">
+                    <input
+                      type="file"
+                      class="custom-file-input"
+                      name="_image"
+                      id="setting-biodata-image-import"
+                      accept="image/x-png, image/jpeg"
+                      @change="autoUploadImageOnChange"
+                    />
+                    <label
+                      class="custom-file-label"
+                      for="setting-biodata-image-import"
+                    >{{imageUploadOrigin}}</label>
+                  </div>
+                  <input
+                    type="text"
+                    id="setting-biodata-image-src"
+                    v-model="imageUploadForm"
+                    hidden
+                    readonly
+                  />
+                  <input
+                    type="submit"
+                    class="btn btn-primary"
+                    id="setting-biodata-image-submit"
+                    style="display: none;"
+                  />
+                </form>
+                <p
+                  class="text-muted mt-1"
+                  style="font-size: 12px"
+                >Image rule: photo dimensions 4x4, format [jpg, jpeg, png], max size 2 MB.</p>
+                <img
+                  v-if="this.imageUploadResult"
+                  class="profile-user-img img-fluid img-circle"
+                  :src="`${this.$AppHelper.apiEndpoint()}${this.imageUploadResult}`"
+                  alt="user-profile-image"
+                />
+              </div>
+              <button class="btn btn-primary float-right text-bold">
+                <i class="fas fa-user-alt"></i>&ensp;Update Biodata
+              </button>
+            </div>
+            <div class="tab-pane" id="password">
+              <p class="text-bold text-center">--- Password Management ---</p>
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="setting-password-current"
+                  placeholder="Current Password"
+                />
+                <input
+                  type="password"
+                  class="form-control mt-3"
+                  id="setting-password-new"
+                  placeholder="New Password"
+                />
+              </div>
+              <button class="btn btn-primary float-right text-bold">
+                <i class="fas fa-key"></i>&ensp;Update Password
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -47,10 +130,54 @@
 
 <script>
 import { mapGetters } from "vuex";
+import Toastr from "@/third-party/library/toastrjs.min";
 export default {
   name: "ProfilePage",
   computed: {
     ...mapGetters(["thisBiodata"])
+  },
+  methods: {
+    autoUploadImageOnChange() {
+      this.$("#setting-biodata-image-submit").click();
+    },
+    uploadImageProfile(e) {
+      e.preventDefault();
+      this.$axios.get(`/sanctum/csrf-cookie`).then(() => {
+        let formImage = new FormData(
+          document.getElementById("setting-biodata-image")
+        );
+        this.$axios
+          .post(
+            `/api/user/profile?_upload=image`,
+            formImage,
+            this.$CredMng.axiosHeaderToken()
+          )
+          .then(res => {
+            if (res.data.status == "success") {
+              this.imageUploadOrigin = res.data.response_data[0].origin_name;
+              this.imageUploadForm = res.data.response_data[0].img_name;
+              this.imageUploadResult = res.data.response_data[0].img_name;
+            } else {
+              this.imageUploadOrigin = "";
+              this.imageUploadForm = "";
+              this.imageUploadResult = "";
+              Toastr.toastError(`${res.data.message._image[0]}`);
+            }
+          })
+          .catch(err => {
+            let error = err.toJSON();
+            Toastr.toastError(error.message);
+          });
+      });
+    }
+  },
+  data() {
+    return {
+      fullNameBiodata: this.$("#setting-biodata-name").val(),
+      imageUploadOrigin: "",
+      imageUploadForm: "",
+      imageUploadResult: ""
+    };
   }
 };
 </script>
